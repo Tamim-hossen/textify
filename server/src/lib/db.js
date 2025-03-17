93945627e4968a3f23663b9a31c64049ccfec08e
Tamim <tamimhossen003@gmail.com>
2025-03-17 17:51:10 +0600

    check

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -5,8 +5,10 @@ dotenv.config();
 
 export const connectDB = async()=>{
     try {
-       const conn =  await mongoose.connect(process.env.MONGODB_URI);
-       console.log(`MongoDB Connected : ${conn.connection.host}`)
+        console.log("🔍 MONGO_URI:", process.env.MONGODB_URI);
+
+    //    const conn =  await mongoose.connect(process.env.MONGODB_URI);
+    //    console.log(`MongoDB Connected : ${conn.connection.host}`)
     } catch (error) {
         console.log("MongoDB Connection Error: ", error)
         process.exit(1)
